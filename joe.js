ee8a78c8b530b6f7757c383adce558372e15e973
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-07-22 11:31:57 -0700

    adding data persistence

--- joe.js
+++ joe.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path")
 const app = express();
+const fs = require("fs")
 
-const pets = require("./pets.json")
+
 
 //middleware for find static assets
 app.use(express.static("public"))
@@ -16,29 +17,49 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/pets",(req,res)=>{
-    res.json(pets)
+   fs.readFile("./pets.json","utf8",(err,data)=>{
+    if(err){
+        throw err
+    } else {
+        const pets = JSON.parse(data);
+        res.json(pets)
+    }
+   })
 })
 
 app.post("/pets",(req,res)=>{
     console.log(req.body);
-    pets.push({
+    const newPet = {
         id:req.body.id,
         name:req.body.name,
         species:req.body.species,
         owner:req.body.owner
-    })
-    res.json({data:req.body,message:"success!"})
+    }
+    fs.readFile("./pets.json","utf8",(err,data)=>{
+        if(err){
+            throw err
+        } else {
+            const pets = JSON.parse(data);
+            pets.push(newPet)
+            fs.writeFile("./pets.json",JSON.stringify(pets,null,4),(err,data)=>{
+                if(err) {
+                    throw err;
+                }
+                res.json({data:req.body,message:"success!"})
+            })
+        }
+       })
 })
 
-app.get("/pets/:petName",(req,res)=>{
-    let thisPet;
-    pets.forEach(pet=>{
-        if(pet.name.toLowerCase()===req.params.petName.toLowerCase()){
-            thisPet = pet;
-        }
-    })
-    res.json(thisPet)
-})
+// app.get("/pets/:petName",(req,res)=>{
+//     let thisPet;
+//     pets.forEach(pet=>{
+//         if(pet.name.toLowerCase()===req.params.petName.toLowerCase()){
+//             thisPet = pet;
+//         }
+//     })
+//     res.json(thisPet)
+// })
 
 app.listen(3000,()=>{
     console.log("listenin to port " + 3000)
