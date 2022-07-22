179095870999a799527cec7bf7a4339ded21f1a7
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-07-22 14:03:13 -0700

    adding heroku config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 //middleware for find static assets
 app.use(express.static("public"));
 
@@ -16,8 +16,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("listenin to port " + 3000);
+app.listen(PORT, () => {
+  console.log("listenin to port " + PORT);
 });
 
 
