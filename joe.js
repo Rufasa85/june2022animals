44901f0abb39d9d513adada410fa0244e3e001a6
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-07-22 10:21:39 -0700

    adding post requests

--- joe.js
+++ joe.js
@@ -4,10 +4,14 @@ const app = express();
 
 const pets = require("./pets.json")
 
+//middleware for find static assets
 app.use(express.static("public"))
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/",(req,res)=>{
-    // res.send("<h1>Welcome to my site! Isnt it grand?</h1>")
     res.sendFile(path.join(__dirname,"./index.html"))
 })
 
@@ -16,12 +20,17 @@ app.get("/pets",(req,res)=>{
 })
 
 app.post("/pets",(req,res)=>{
+    console.log(req.body);
+    pets.push({
+        id:req.body.id,
+        name:req.body.name,
+        species:req.body.species,
+        owner:req.body.owner
+    })
     res.send("you made a post request!")
 })
 
 app.get("/pets/:petName",(req,res)=>{
-    console.log(req);
-    // res.json(req.params)
     let thisPet;
     pets.forEach(pet=>{
         if(pet.name.toLowerCase()===req.params.petName.toLowerCase()){
